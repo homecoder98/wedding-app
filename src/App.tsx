1651d7f778ae4839fc083b84a978f92b555372a2
homecoder98 <homecoder98@gmail.com>
2024-05-08 21:34:53 +0900

    Feat: 목업 더미 데이터 받아오기 전 처리 로딩중일때 state처리해 하트 애니메이션 에러발생시 state처리해 에러 화면 노출

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,39 @@
 import React, { useEffect, useState } from 'react'
-import styled from 'styled-components'
-
-const Container = styled.div`
-  background-color: #ea7664;
-`
+import { Container } from '@/styles/App-styled'
+import FullScreenMessage from '@/components/shared/FullScreenMessage'
 
 function App() {
   const [wedding, setWedding] = useState('')
   const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
 
   useEffect(() => {
     setLoading(true)
 
     fetch('http://localhost:8888/wedding')
       .then((response) => {
+        if (response.ok === false) {
+          throw new Error('청첩장 정보를 불러오지 못했습니다')
+        }
         return response.json()
       })
       .then((data) => {
         setWedding(data)
+      })
+      .catch((e) => {
+        setError(true)
+      })
+      .finally(() => {
         setLoading(false)
       })
   }, [])
 
   if (loading) {
-    return <div>Loading...</div>
+    return <FullScreenMessage type="loading" />
+  }
+
+  if (error) {
+    return <FullScreenMessage type="error" />
   }
 
   return <Container>{JSON.stringify(wedding)}</Container>
